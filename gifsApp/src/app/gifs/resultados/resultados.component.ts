8cb675caa40a1ff6336c5b51e8f8b379a105964f
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-13 15:00:06 +0200

    86-colocando-tipado-peticiones-htp

--- gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gif } from '../interface/gifs.interface';
 import { GifsService } from '../services/gifs.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { GifsService } from '../services/gifs.service';
 })
 export class ResultadosComponent {
 
-  get resultados() {
+  get resultados() : Gif[] {
     return this.gifsService.resultados;
   }
 
--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   // esto es para que esté disponible de forma global en toda la app
@@ -11,7 +12,7 @@ export class GifsService {
   private _historial: string[] = [];
 
   // TODO cambiar any por su tipo correspondiente
-  public resultados: any[] = []
+  public resultados: Gif[] = []
 
   get getHistorial() {
     return [...this._historial]
@@ -28,8 +29,8 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=ERRUFlHbghUkFTtTl1ipwfPh0Uir8625&q=${query}&limit=10`)
-      .subscribe((resp: any) => {
+    this.http.get<SearchGifResponse>(`https://api.giphy.com/v1/gifs/search?api_key=ERRUFlHbghUkFTtTl1ipwfPh0Uir8625&q=${query}&limit=10`)
+      .subscribe((resp) => {
         console.log(resp);
         this.resultados = resp.data
       })
